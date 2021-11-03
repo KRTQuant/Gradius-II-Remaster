cf51a0c14a3a604a54f832a763b3ba49f63eae1b
KRTQuant <61773202+KRTQuant@users.noreply.github.com>
2021-11-04 02:35:40 +0700

    Finish bullet collide and play anim

--- Assets/Script/Bullet/ArmamentControl.cs
+++ Assets/Script/Bullet/ArmamentControl.cs
@@ -12,20 +12,24 @@ public class ArmamentControl : MonoBehaviour
     [SerializeField] private float waitTime; //wait time before disappear
     [SerializeField] public enum AmmoType { SINGLE, PULSE, BEAM, TAIL, SECOND, MISSILE, SPREAD, TORPEDO, TWOWAYUP, TWOWAYDOWN }
     [SerializeField] public AmmoType currentAmmoType;
+    [SerializeField] private bool isStop;
 
     [SerializeField] private GameObject player;
+    [SerializeField] private Animator animator;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         player = GameObject.Find("Player");
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
         HandleMovement();
         HandleOutOfRange();
+        Disable();
     }
 
     void OnBecameInvisible()
@@ -35,49 +39,52 @@ public class ArmamentControl : MonoBehaviour
 
     private void HandleMovement()
     {
-        if (currentAmmoType == AmmoType.PULSE)
+        if(!isStop)
         {
-            rb.velocity = new Vector2(speed, rb.velocity.y);
-        }
-        if (currentAmmoType == AmmoType.BEAM)
-        {
-            rb.velocity = new Vector2(speed, rb.velocity.y);
-            transform.position = new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
-        }
-        if (currentAmmoType == AmmoType.TAIL)
-        {
-            //Debug.Log("Tail: " + rb.velocity.x);
-            rb.velocity = new Vector2(-speed, rb.velocity.y);
-        }
-        if (currentAmmoType == AmmoType.SECOND)
-        {
-            rb.velocity = VectorFromAngle(45) * speed;
-        }
-        if (currentAmmoType == AmmoType.MISSILE)
-        {
-            rb.velocity = VectorFromAngle(315) * speed;
-        }
-        if (currentAmmoType == AmmoType.SPREAD)
-        {
-            rb.gravityScale = 1;
-            rb.velocity = new Vector2(speed, rb.velocity.y);
-        }
-        if (currentAmmoType == AmmoType.TORPEDO)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, speed);
-            //Debug.Log(rb.velocity);
-        }
-        if (currentAmmoType == AmmoType.TWOWAYUP)
-        {
-            rb.velocity = VectorFromAngle(45) * speed;
-        }
-        if (currentAmmoType == AmmoType.TWOWAYDOWN)
-        {
-            rb.velocity = VectorFromAngle(-45) * speed;
-        }
-        if (currentAmmoType == AmmoType.SINGLE)
-        {
-            rb.velocity = new Vector2(speed, rb.velocity.y);
+            if (currentAmmoType == AmmoType.PULSE)
+            {
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+            }
+            if (currentAmmoType == AmmoType.BEAM)
+            {
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+                transform.position = new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
+            }
+            if (currentAmmoType == AmmoType.TAIL)
+            {
+                //Debug.Log("Tail: " + rb.velocity.x);
+                rb.velocity = new Vector2(-speed, rb.velocity.y);
+            }
+            if (currentAmmoType == AmmoType.SECOND)
+            {
+                rb.velocity = VectorFromAngle(45) * speed;
+            }
+            if (currentAmmoType == AmmoType.MISSILE)
+            {
+                rb.velocity = VectorFromAngle(315) * speed;
+            }
+            if (currentAmmoType == AmmoType.SPREAD)
+            {
+                rb.gravityScale = 1;
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+            }
+            if (currentAmmoType == AmmoType.TORPEDO)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, speed);
+                //Debug.Log(rb.velocity);
+            }
+            if (currentAmmoType == AmmoType.TWOWAYUP)
+            {
+                rb.velocity = VectorFromAngle(45) * speed;
+            }
+            if (currentAmmoType == AmmoType.TWOWAYDOWN)
+            {
+                rb.velocity = VectorFromAngle(-45) * speed;
+            }
+            if (currentAmmoType == AmmoType.SINGLE)
+            {
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+            }
         }
     }
 
@@ -162,5 +169,34 @@ public class ArmamentControl : MonoBehaviour
     {
         yield return new WaitForSeconds(waitTime);
 /*        this.gameObject.SetActive(false);*/
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Obstacle") || collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Collide with player");
+            isStop = true;
+            rb.velocity = Vector2.zero;
+            Debug.Log(isStop);
+            RuntimeAnimatorController rac = Resources.Load("SurfaceShooting") as RuntimeAnimatorController;
+            animator.runtimeAnimatorController = rac;
+            animator.Play("SurfaceShootingEffect");
+        }
+    }
+
+    private void OnDisable()
+    {
+        isStop = false;
+        animator.runtimeAnimatorController = null;
+        sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 255);
+    }
+
+    private void Disable()
+    {
+        if(sprite.color.a == 0)
+        {
+            this.gameObject.SetActive(false);
+        }
     }
 }
