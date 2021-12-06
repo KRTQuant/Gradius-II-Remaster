3ce6e473dd058365e6a634343aa507334ab1a34e
KRTQuant <61773202+KRTQuant@users.noreply.github.com>
2021-12-06 19:21:05 +0700

    Update Solar Flare Collision

--- Assets/Script/Scene/UpdatePolygonCollider.cs
+++ Assets/Script/Scene/UpdatePolygonCollider.cs
@@ -6,6 +6,7 @@ public class UpdatePolygonCollider : MonoBehaviour
 {
     [SerializeField] private PolygonCollider2D polygon;
     [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite currentSprite;
 
     //[SerializeField] private 
 
@@ -13,6 +14,17 @@ public class UpdatePolygonCollider : MonoBehaviour
     {
         polygon = GetComponent<PolygonCollider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        currentSprite = spriteRenderer.sprite;
+    }
+
+    private void Update()
+    {
+        if(currentSprite != spriteRenderer.sprite)
+        {
+            Destroy(polygon);
+            currentSprite = spriteRenderer.sprite;
+            polygon = gameObject.AddComponent<PolygonCollider2D>();
+        }
     }
 
 
